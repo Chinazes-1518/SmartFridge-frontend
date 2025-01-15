a83eb81f3b713fa5e12ce0c446d5e21bf27fd7b9
Vyacheslav <76677694+flyare1337@users.noreply.github.com>
2025-01-15 16:39:53 +0300

    initial root

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="root" id="root">
+    <router-view v-slot="{ Component, route }">
+      <div class="layout">
+        <transition name="page" mode="out-in">
+          <component :is="Component" :key="route.fullPath" />
+        </transition>
+      </div>
+    </router-view>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.root {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.layout {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  flex: 1;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,16 @@
+// @ts-nocheck
+
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: () => import('@/views/Index.vue'),
+        }
+    ]
+})
+
+export default router
