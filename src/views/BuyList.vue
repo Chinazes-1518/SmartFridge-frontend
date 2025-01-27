cd8bc82a18c684a5ecc22bc4ef80b7ab645c3e47
Vyacheslav <76677694+flyare1337@users.noreply.github.com>
2025-01-27 20:45:04 +0300

    Update BuyList.vue

--- src/views/BuyList.vue
+++ src/views/BuyList.vue
@@ -17,13 +17,43 @@ async function loadBuyList() {
   }
 }
 
+async function getProductType(id) {
+  const data = await APIRequest(`/product_types/get`, "GET", {
+    id: id
+  }, {}, true)
+
+  if (data.status === 200) {
+    console.log(data.json)
+    return (data.json)
+  }
+
+  return 0
+}
+
 </script>
 
 <template>
   <div class="">
     <div class="buy">
       <div class="buy-container container">
-        {{ buy }}
+        <div class="buy-table-pre">
+          <table class="buy-table">
+            <thead>
+            <tr class="buy-table-tr">
+              <th class="buy-table-th">ID</th>
+              <th class="buy-table-th">Название продукта</th>
+              <th class="buy-table-th">Масса</th>
+              <th class="buy-table-th">Действия</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr class="buy-table-tr" v-for="item in Object.values(buy)[0]">
+              <td class="buy-table-td">{{ item.id }}</td>
+              <td class="buy-table-td">{{ getProductType(item.prod_type_id) }} ({{ item.prod_type_id }})</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </div>
