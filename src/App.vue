98b4207984e8d71743242fed856a50bac8f14348
Vyacheslav <76677694+flyare1337@users.noreply.github.com>
2025-01-27 18:16:56 +0300

    new qr scanner design

--- src/App.vue
+++ src/App.vue
@@ -11,33 +11,45 @@
           <component :is="Component" :key="route.fullPath" />
         </transition>
       </div>
-      <Modal id="qr_scan" title="Сканер QR">
+      <Modal id="qr_scan" title="Сканнер QR">
         <!-- {{ dataref }} -->
         <div v-if="dataref === ''">
           <QrScanner :fps="10" :qrbox="200" :on-scanned="onScanSuccess"></QrScanner>
         </div>
-        <div v-if="dataref !== ''">
-          <span class="qr-scanner-name">{{ dataref.type_name }} ({{ dataref.cat_name }})</span>
+        <div v-if="dataref !== ''" class="qr-scanner-info">
+          <div class="qr-scanner-info-title"><PhInfo :size="25" /> Информация о продукте</div>
+          <div class="qr-scanner-info-pos">
+            <div class="qr-scanner-name name">{{ dataref.type_name }} ({{ dataref.cat_name }})</div>
+          </div>
+          <div class="qr-scanner-info-pos">
+            <div class="qr-scanner-name">ID продукта:</div> <div class="qr-scanner-value"><code>{{ dataref.prod_id }}</code></div>
+          </div>
+          <div class="qr-scanner-info-pos">
+            <div class="qr-scanner-name">Дата производства:</div> <div class="qr-scanner-value">{{ dataref.production_date }}</div>
+          </div>
+          <div class="qr-scanner-info-pos">
+            <div class="qr-scanner-name">Срок годности:</div> <div class="qr-scanner-value">{{ dataref.expiry_date }}</div>
+          </div>
+          <div class="qr-scanner-info-pos">
+            <div class="qr-scanner-name">Количество:</div> <span class="qr-scanner-value"><code>{{ dataref.amount }}</code> {{ dataref.units }}</span>
+          </div>
+          <div class="qr-scanner-info-pos">
+            <div class="qr-scanner-name">Пищевая ценность:</div> <div class="qr-scanner-value"><code>{{ dataref.nutritional }}</code> ккал</div>
+          </div>
+          <div class="qr-scanner-info-pos">
+            <div class="qr-scanner-name">Единицы измерения:</div> <div class="qr-scanner-value">{{ dataref.measure_type }}</div>
+          </div>
+          <div class="qr-scanner-info-pos">
+            <div class="qr-scanner-name">Особенности:</div> <div class="qr-scanner-value" v-if="dataref.allergens !== null">{{ dataref.allergens.split(',').map((x) => specs[x]).join(', ') }}</div><span v-if="dataref.allergens === null">нет</span>
+          </div>
           <br>
-          <span class="qr-scanner-name">ID продукта</span>: <span class="qr-scanner-value">{{ dataref.prod_id }}</span>
-          <br>
-          <span class="qr-scanner-name">Дата производства</span>: <span class="qr-scanner-value">{{ dataref.production_date }}</span>
-          <br>
-          <span class="qr-scanner-name">Срок годности</span>: <span class="qr-scanner-value">{{ dataref.expiry_date }}</span>
-          <br>
-          <span class="qr-scanner-name">Количество</span>: <span class="qr-scanner-value">{{ dataref.amount }} {{ dataref.units }}</span>
-          <br>
-          <span class="qr-scanner-name">Пищевая ценность</span>: <span class="qr-scanner-value">{{ dataref.nutritional }} ккал</span>
-          <br>
-          <span class="qr-scanner-name">Единицы измерения</span>: <span class="qr-scanner-value">{{ dataref.measure_type }}</span>
-          <br>
-          <span class="qr-scanner-name">Особенности</span>: <span class="qr-scanner-value" v-if="dataref.allergens !== null">{{ dataref.allergens.split(',').map((x) => specs[x]).join(', ') }}</span><span v-if="dataref.allergens === null">нет</span>
-          <br>
-          <br>
-          <div style="display: flex; column-gap: 10px;">
-            <button @click="dataref = ''" class="qr-scanner-button">Сканировать заново</button>
-            <button @click="" class="qr-scanner-button blue"><PhKnife :size="25" /></button>
-            <button @click="" class="qr-scanner-button green"><PhBasket :size="25" /></button>
+          <div class="qr-scanner-info-title"><PhHandTap :size="25" /> Действия</div>
+          <div class="qr-scanner-buttons">
+            <button @click="" class="qr-scanner-button blue"><PhKnife :size="25" />Приготовить</button>
+            <button @click="" class="qr-scanner-button green"><PhPlusCircle :size="25" />В холодильник</button>
+<!--            <button @click="" class="qr-scanner-button red"><PhTrash :size="25" />В мусорку</button>-->
+            <button @click="" class="qr-scanner-button purple"><PhBasket :size="25" />В список покупок</button>
+            <button @click="dataref = ''" class="qr-scanner-button yellow"><PhArrowCounterClockwise :size="22" /> Сканировать заново</button>
           </div>
         </div>
       </Modal>
@@ -69,7 +81,7 @@ import {
   PhLightning,
   PhRuler,
   PhSparkle,
-  PhListMagnifyingGlass, PhBinoculars, PhKnife, PhQrCode
+  PhListMagnifyingGlass, PhBinoculars, PhKnife, PhQrCode, PhArrowCounterClockwise, PhPlusCircle, PhInfo, PhHandTap
 } from "@phosphor-icons/vue";
 
 let specs = {
@@ -113,23 +125,66 @@ function onScanSuccess(text: string, error: any) {
 .qr-scanner {
   &-name {
     font-weight: 400;
+
+    &.name {
+      font-weight: 600;
+    }
+  }
+
+  &-info {
+    display: flex;
+    flex-direction: column;
+    gap: 5px 0;
+
+    &-pos {
+      display: flex;
+      gap: 0 10px;
+      //margin-left: 38px;
+    }
+
+    &-title {
+      font-weight: 600;
+      display: flex;
+      align-items: center;
+      gap: 0 13px;
+      margin-bottom: 10px;
+    }
   }
 
   &-value {
-    font-family: monospace;
+
+    & code {
+      font-family: monospace;
+    }
+
   }
+
+  &-buttons {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 10px;
+    //margin-left: 38px;
+
+
+    @media (max-width: 580px) {
+      grid-template-columns: 1fr;
+    }
+  }
+  
 
   &-button {
     width: 100%;
+    height: 50px;
     text-align: center;
     padding: 5px 15px;
     border-radius: 15px;
     transition: .1s ease-in;
     justify-content: center;
-    background-color: #fcc697 !important;
-    border: 1px solid #fd9a2f;
+    //background-color: #fcc697 !important;
+    //border: 1px solid #fd9a2f;
     display: flex;
     align-items: center;
+    gap: 0 9px;
 
     &.blue {
       border: 1px solid rgb(55, 109, 255, 0.25) !important;
@@ -146,6 +201,42 @@ function onScanSuccess(text: string, error: any) {
 
       &:hover {
         background: rgb(0, 204, 0, 0.25) !important;
+      }
+    }
+
+    &.red {
+      border: 1px solid rgb(255, 59, 59, 0.25);
+      background: rgb(255, 59, 59, 0.2);
+
+      &:hover {
+        background: rgb(255, 59, 59, 0.25);
+      }
+    }
+
+    &.white {
+      background: rgba(255, 255, 255, 0.4);
+      border: 1px solid rgba(255, 255, 255, 0.5);
+
+      &:hover {
+        background: rgba(255, 255, 255, 0.3);
+      }
+    }
+
+    &.purple {
+      background: rgb(238, 50, 255, 0.2);
+      border: 1px solid rgb(238, 50, 255, 0.25);
+
+      &:hover {
+        background: rgb(238, 50, 255, 0.25);
+      }
+    }
+
+    &.yellow {
+      background: rgba(211, 170, 20, 0.2);
+      border: 1px solid rgba(211, 170, 20, 0.25);
+
+      &:hover {
+        background: rgba(211, 170, 20, 0.25);
       }
     }
   }
