3ee5579ff5e02640e50ce98a175028438e5a6ed4
JaanDev <67502867+JaanDev@users.noreply.github.com>
2025-02-10 00:50:38 +0300

    little thingy

--- src/modals/QrShowModal.vue
+++ src/modals/QrShowModal.vue
@@ -4,6 +4,7 @@
     <div style="display: flex" v-if="$store.state.qrGenerated">
       <qrcode-vue :value="$store.state.qrData" :size="300" render-as="svg" level="H" background="#ffffff00" style="margin: 0 auto"/>
     </div>
+    <p style="margin: auto; text-align: center; margin-top: 20px; font-size: 1.2rem;">ID продукта: {{ $store.state.currentProduct }}</p>
   </Modal>
 </template>
 
